f1fb3a6bbbae6756c4c93162a4e9f26042575eb7
phonggiotai <114666846+phonggiotai@users.noreply.github.com>
2025-03-14 12:34:00 +0700

    Add environment variables for Firebase configuration;

--- frontend/my-react-app/src/components/firebase/firebaseConfig.jsx
+++ frontend/my-react-app/src/components/firebase/firebaseConfig.jsx
@@ -1,16 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDZAvHpW7xvbtI9dtFpepQlfvW0ghPzi6Y",
-    authDomain: "greeting-card-web.firebaseapp.com",
-    projectId: "greeting-card-web",
-    storageBucket: "greeting-card-web.firebasestorage.app",
-    messagingSenderId: "383956683019",
-    appId: "1:383956683019:web:5c90131021564a693c7a0e",
-    measurementId: "G-04BP8L6YLG"
-  };
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
